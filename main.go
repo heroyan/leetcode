e66ae518e25e83f427d6b833118d58deacf29828
heroyan <zndxysf@126.com>
2020-06-04 00:59:44 +0800

    概率问题

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import "leetcode/problems"
 func main()  {
 	//problems.TestQ()
 	//problems.FourSum()
-	problems.P64()
+	problems.P837()
 	//fmt.Println(problems.FindMedianSortedArrays2([]int{1,2,3,4}, []int{5,6,7,8,9,10}))
 }
