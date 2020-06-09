f7f8bd231ca47ab9ad701d9076429f9cea32500c
heroyan <zndxysf@126.com>
2020-06-10 00:58:52 +0800

    P9 P990

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import "leetcode/problems"
 func main()  {
 	//problems.TestQ()
 	//problems.FourSum()
-	problems.P837()
+	problems.P9()
 	//fmt.Println(problems.FindMedianSortedArrays2([]int{1,2,3,4}, []int{5,6,7,8,9,10}))
 }
